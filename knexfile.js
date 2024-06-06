2cee2e451800e25d44294069cafaa4e4f6bebeef
Ilyas Assakali <ilyas.assakali@student.ehb.be>
2024-06-06 23:24:58 +0200

    Added host and port for knexfile

--- knexfile.js
+++ knexfile.js
@@ -10,6 +10,8 @@ module.exports = {
       database: process.env.DATABASE,
       user: process.env.USER,
       password: process.env.PASSWORD,
+      host: process.env.HOST,
+      port: process.env.PORT,
     },
   },
 };
